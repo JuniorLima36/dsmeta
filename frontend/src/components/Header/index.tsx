a7309682f2be8031a8a6bac96ae5c22e25b1024d
JuniorLima36 <juninho_livre2011@hotmail.com>
2022-11-20 00:24:50 -0300

    Other components

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,7 +1,19 @@
+import logo from '../../assets/img/logo.svg';
+import './styles.css';
+
 function Header() {
-    return (
-      <p></p>
-    )
-  }
-  
-  export default Header;
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://github.com/JuniorLima36"> JuniorLima36</a>
+        </p>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
